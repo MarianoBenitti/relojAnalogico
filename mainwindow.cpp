2b70cffe4e1540be359c4dff917223da3fc738d8
MarianoBenitti <benittimariano@hotmail.com>
2023-08-17 00:27:08 -0300

    Generacion de fondo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+//paleta de colores
+#define GRISBASE 0x3C414F
+#define CELESTEBASE 0x05B2A3
+
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -19,19 +24,48 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::TimerGen1(){
+
     QPainter QpainterReloj(QpaintReloj->getCanvas());
     QPen pen;
     QBrush brush;
-    pen.setColor(Qt::cyan);
-    pen.setWidth(3);
-    brush.setColor(Qt::darkGray);
+    QPoint trapecio[4];
+    trapecio[0].setX(-50);
+    trapecio[0].setY(-225);
+    trapecio[1].setX(50);
+    trapecio[1].setY(-225);
+    trapecio[2].setX(30);
+    trapecio[2].setY(-180);
+    trapecio[3].setX(-30);
+    trapecio[3].setY(-180);
 
+
+    QPoint centro;
+    centro.setX(0);
+    centro.setY(-200);
+    int i;
+    //se dibuja el circulo central del reloj
+    pen.setColor(CELESTEBASE);
+    pen.setWidth(7);
     brush.setStyle(Qt::SolidPattern);
-    brush.gradient();
+    brush.setColor(Qt::darkGray);
+    brush.setColor(GRISBASE);
     QpainterReloj.setPen(pen);
     QpainterReloj.setBrush(brush);
     QpainterReloj.drawEllipse(10,10,480,480);
+    //dibujamos los circulos donde se encontraran las horas
+    QpainterReloj.translate(250,250);
+    brush.setColor(Qt::darkMagenta);
+    brush.setStyle(Qt::Dense3Pattern);
+    for(i=1;i<=12;i++){
+        QpainterReloj.rotate(30);
+        if(i%3){
+            QpainterReloj.setBrush(Qt::NoBrush);
+            QpainterReloj.drawEllipse(centro,28,28);
+        }else{
+            QpainterReloj.setBrush(brush);
+            QpainterReloj.drawPolygon(trapecio,4);
+        }
+    }
 
-    QpaintReloj->update();
-
+    QpaintReloj->update();   
 }
